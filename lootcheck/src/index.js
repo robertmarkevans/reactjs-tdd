4487467b860edce32175fcef9cdc592e7dfcc44b
rmevans <robertmarkevans@gmail.com>
2019-03-28 11:47:24 +0000

    Store away, no idea what state it's in

---
+++ lootcheck/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+import { createStore } from 'redux'
+import rootReducer from './reducers/balance'
+import { Provider } from 'react-redux'
+
+const store = createStore(rootReducer)
+
+render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById('root'))
